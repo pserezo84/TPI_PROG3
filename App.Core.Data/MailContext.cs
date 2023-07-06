903d4ec1f5a6bb0083e2e209a1d27a7d7557b4fe
Pablo Serezo <pserezo@gmail.com>
2023-07-06 00:19:19 -0300

    Creacion de Context y repository en capa acceso a datos

---
+++ App.Core.Data/MailContext.cs
@@ -0,0 +1,41 @@
+﻿using App.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Core.Data
+{
+    public class MailContext:DbContext
+    {
+        
+        public DbSet<Mail> Mails { get; set; }
+
+        public DbSet<Destinatario> Destinatarios { get; set; }
+
+        public DbSet<Contacto> Contactos { get; set; }
+
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    string connectionString =
+        //        "Persist Security Info=True;Initial Catalog=webmail;Data Source=.; Integrated Security=True;TrustServerCertificate=True;";
+        //    optionsBuilder.UseSqlServer(connectionString);
+        //}
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                string connectionString = "Persist Security Info=True;Initial Catalog=webmail;Data Source=.; Integrated Security=True;TrustServerCertificate=True;";
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+    }
+}
---
+++ App.Core.Data/MailRepository.cs
@@ -0,0 +1,63 @@
+﻿using App.Core.Entities;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+
+namespace App.Core.Data
+{
+    public class MailRepository
+    {
+        public MailContext dbContext { get; set; }
+        
+        public MailRepository(MailContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public MailRepository()
+        {
+        }
+
+        public List<Mail> Search(string textToSearch)
+        {
+
+            using (var context = new MailContext())
+            {
+                var query = from m in context.Mails
+                            where m.Asunto.Contains(textToSearch)
+                            select m;
+
+                return query.ToList();
+            }
+
+        }
+
+        public IEnumerable<Mail> GetAll()
+        {
+            using (var context = new MailContext())
+            {
+                return context.Mails.ToList();
+                //return dbContext.Mails.ToList();
+            }
+            
+        }
+        public Mail GetById(int id)
+        {
+            using (var context = new MailContext())
+            {
+                return dbContext.Mails.Find(id);
+            }
+        }
+
+        public void Add(Mail mail)
+        {
+            using (var context = new MailContext())
+            {
+                dbContext.Mails.Add(mail);
+                dbContext.SaveChanges();
+            }
+        }
+
+    }
+
+    
+}
