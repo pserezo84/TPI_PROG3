6375bb996f9d80579f565c65f5e393b4e9e25b78
Pablo Serezo <pserezo@gmail.com>
2023-07-06 00:20:41 -0300

    Creacion de IMailService y MailService en capa de negocios

---
+++ App.Core.Business/Interfaces/IMailService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App.Core.Entities;
+using App.Core.Business.Services;
+
+namespace App.Core.Business.Interfaces
+{
+    internal interface IMailService
+    {
+        IEnumerable<Mail> GetAllMails();
+        Mail GetMailById(int id);
+        void AddMail(Mail mail);
+    }
+}
---
+++ App.Core.Business/Services/MailService.cs
@@ -0,0 +1,42 @@
+﻿using App.Core.Business.Interfaces;
+using App.Core.Data;
+using App.Core.Entities;
+
+namespace App.Core.Business.Services
+{
+    public class MailService: IMailService
+    {
+        private readonly MailRepository _mailRepository;
+
+
+        public MailService()
+        {
+            _mailRepository = new MailRepository();
+        }
+
+
+        //public List<Mail> Search(string textToSearch) {
+
+        //    //Validar textToSearch
+        //    //Paginar
+
+        //    return _mailRepository.Search(textToSearch); ;
+        //}
+
+        public IEnumerable<Mail> GetAllMails()
+        {
+            return _mailRepository.GetAll();
+        }
+
+        public Mail GetMailById(int id)
+        {
+            return _mailRepository.GetById(id); ;
+        }
+
+        public void AddMail(Mail mail)
+        {
+            _mailRepository.Add(mail);
+        }
+
+    }
+}
